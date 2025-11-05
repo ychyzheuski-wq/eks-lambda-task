e122a72838172a5fd4fa44f1d9349e07287e942d
Yahor Chyzheuski <ychyzheuski@rocketsoftware.com>
2025-11-05 21:20:10 +0100

    Created AWS CDK project, added code to provision EKS/VPC/SSM

--- aws_cdk_project/aws_cdk_project_stack.py
+++ aws_cdk_project/aws_cdk_project_stack.py
@@ -1,8 +1,12 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ssm as ssm,
+    aws_ec2 as ec2,
+    aws_eks as eks,
+    CfnOutput
 )
+
+from aws_cdk.lambda_layer_kubectl_v32 import KubectlV32Layer
 from constructs import Construct
 
 class AwsCdkProjectStack(Stack):
@@ -10,10 +14,46 @@ class AwsCdkProjectStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        ssm_parameter = ssm.StringParameter(
+            self, "ParameterAccountEnv",
+            parameter_name="/platform/account/env",
+            string_value="development",
+            description="Setting the environment development / staging / production for the account." 
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        vpc = ec2.Vpc(
+            self, "VPC", 
+            ip_addresses=ec2.IpAddresses.cidr("10.102.0.0/16"),
+            max_azs=2,
+            subnet_configuration=[
+                    ec2.SubnetConfiguration(
+                        name="Public",
+                        subnet_type=ec2.SubnetType.PUBLIC,
+                        cidr_mask=24
+                    ),
+                    ec2.SubnetConfiguration(
+                        name="Private",
+                        subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                        cidr_mask=24
+                    )
+            ]
+        )
+
+        eks_cluster = eks.Cluster(
+            self, "EKSCluster",
+            vpc=vpc,
+            version=eks.KubernetesVersion.V1_32,
+            kubectl_layer=KubectlV32Layer(self, "kubectl"),
+            default_capacity=1,
+            default_capacity_instance=ec2.InstanceType("t3.medium"),
+            endpoint_access=eks.EndpointAccess.PUBLIC,
+            cluster_name="CustomResourceEksCluster"
+        )
+
+        CfnOutput(
+            self, "ClusterNameOutput",
+            value=eks_cluster.cluster_name,
+            description="The EKS Cluster Name"
+        )
+
+
